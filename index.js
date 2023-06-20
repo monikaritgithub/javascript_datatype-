9eace6eb2cbf001f610bfcee1ea2b36444e454da
monikaritgithub <moneekabhandari123@gmail.com>
2023-06-20 23:59:59 +0545

    datatype

---
+++ index.js
@@ -0,0 +1,37 @@
+//string
+var address="string";
+console.log(address);
+
+//Integer
+var DOB=2020;
+console.log(DOB);
+
+// Float
+var age=30.5;
+console.log(age);
+
+// Array
+// studentName is an object of array
+// object is a collection of multiple data or values
+var studentName=["monika","nischal"];
+console.log(studentName);
+
+//Boolean boal
+var Status=true;
+console.log(Status);
+
+var check = false;
+console.log(check);
+
+// Object -> subjects 
+// dictonary -> key : value
+var subjects= {
+    
+    // key : value
+    english:90,
+    science:80
+};
+console.log(subjects);
+
+
+
